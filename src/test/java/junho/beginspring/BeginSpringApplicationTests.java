13f57f1834e761e6810327ad89339ae271c0bb1b
박준호 <junho6667@gmail.com>
2020-09-07 13:13:09 +0900

    first

---
+++ src/test/java/junho/beginspring/BeginSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package junho.beginspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
